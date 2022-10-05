00ae14b39a16ec4e9b4512f1e5728602bd9663f8
gwhsu <41411537+gwhsu@users.noreply.github.com>
2022-10-06 02:06:45 +0800

    make it better

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ access_token = '4b7bb0089203838f6fd1292f03a1e3187bbe7776'
 refresh_token = 'fb20c55a1821b036094fd5a042b9b7446ee52471'
 
 # line bot key
-line_channel_access_token = 'k18x0ObwF8QcHLs7ZuJQ1g538v9Bnok+fBK/U98OqcY8GQJC3dJaOYhv4DVet+jaAh0mhQ6ZMsBfvhgZGDmKuTZ853+f8JkRzS2MhPzDyBvvSr7HPWgJuCUiBLNaqqT4E8tNQP1Vmg+kjk+m5q14XAdB04t89/1O/w1cDnyilFU='
-line_channel_secret = 'ac1f3fd96837af7ddd36763ea551213c'
+line_channel_access_token ='qexgfv854e6bzSoUazwu6x9RM2qx66QFk3gH7Yo51t9+8QQbqOVVK41Srx1mSOGRPt0sqqMqCWElxEz2U5H5zizS2IoiSX2HL+8Q7m3bxC4Wu8Y9tOSmVp4lpYGOkx2YL+gLzv7tCfQ5OxYuXkkTGgdB04t89/1O/w1cDnyilFU='
+line_channel_secret = '305a9df4cb229b9d801eb03d7d81988f'
 # DB
 from pymongo import MongoClient
 client_mongo = MongoClient('mongodb+srv://test:123@cluster0-lefn4.mongodb.net/test?retryWrites=true&w=majority')
