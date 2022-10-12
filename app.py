40970d12c9d5c22429de6f46ab22a742f9869dd2
BarryYan <barry0310yan@gmail.com>
2022-10-12 18:39:31 +0800

    barry test 10/12

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def handle_message(event):
         message = StickerSendMessage(package_id='1', sticker_id='8')
 
     elif '才修' in msg:
-        txt = '才修為什麼不接電話?'
+        txt = '鄭才修為什麼不接電話?'
         message = TextSendMessage(text=txt)
 
     else:
