01fa173b0f20ae360927975d3f02f0cfd025c2d3
Christee <ra2847545015@gmail.com>
2022-10-12 17:20:32 +0800

    789

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def handle_message(event):
         message = Hulan(msg)
 
     elif '幹你娘' in msg:
-        message = StickerSendMessage(package_id='1', sticker_id='6')
+        message = StickerSendMessage(package_id='1', sticker_id='7')
 
     else:
         message = set_msg(msg)
