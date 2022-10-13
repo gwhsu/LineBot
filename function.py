f3634a796a18a30fb1211e3eff66d99b490f7a13
gwhsu <JIMMY3252968@gmail.com>
2022-10-13 09:52:02 +0800

    1013.0951

--- function.py
+++ function.py
@@ -9,7 +9,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
-
+import base64
 from config import client_id, client_secret, access_token, refresh_token, album_id
 mongo_client = MongoClient('mongodb+srv://test:123@cluster0-lefn4.mongodb.net/test?retryWrites=true&w=majority')
 
@@ -135,21 +135,38 @@ def img2anime(img_path):
     post_image = chrome.find_element_by_xpath('//input[@type="file"]').send_keys(img_path)
     alert = chrome.switch_to.alert
     alert.accept()  # accept alert
-    time.sleep(30)
+    time.sleep(25)
     image = chrome.find_element_by_xpath('//*[@id="outputEl"]/div/div/img')
     images_url = image.get_attribute("src")
 
-    # write image to file
-    reponse = requests.get(images_url)
-    if reponse.status_code == 200:
-        with open("output.jpg", "wb") as file:
-            file.write(reponse.content)
+    def get_file_content_chrome(driver, uri):
+        result = driver.execute_async_script("""
+        var uri = arguments[0];
+        var callback = arguments[1];
+        var toBase64 = function(buffer){for(var r,n=new Uint8Array(buffer),t=n.length,a=new Uint8Array(4*Math.ceil(t/3)),i=new Uint8Array(64),o=0,c=0;64>c;++c)i[c]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".charCodeAt(c);for(c=0;t-t%3>c;c+=3,o+=4)r=n[c]<<16|n[c+1]<<8|n[c+2],a[o]=i[r>>18],a[o+1]=i[r>>12&63],a[o+2]=i[r>>6&63],a[o+3]=i[63&r];return t%3===1?(r=n[t-1],a[o]=i[r>>2],a[o+1]=i[r<<4&63],a[o+2]=61,a[o+3]=61):t%3===2&&(r=(n[t-2]<<8)+n[t-1],a[o]=i[r>>10],a[o+1]=i[r>>4&63],a[o+2]=i[r<<2&63],a[o+3]=61),new TextDecoder("ascii").decode(a)};
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function(){ callback(toBase64(xhr.response)) };
+        xhr.onerror = function(){ callback(xhr.status) };
+        xhr.open('GET', uri);
+        xhr.send();
+        """, uri)
+        if type(result) == int:
+            raise Exception("Request failed with status %s" % result)
+        return base64.b64decode(result)
 
-    '''
+    bytes = get_file_content_chrome(chrome, images_url)
+    # write image to file
+    # reponse = requests.get(images_url)
+    # if reponse.status_code == 200:
+    #    with open("output.jpg", "wb") as file:
+    #        file.write(reponse.content)
+
+    imgdata = base64.b64decode(bytes)
     with open('output.png', 'wb') as file:
-        image = chrome.find_element_by_xpath('//*[@id="outputEl"]/div/div/img')
-        file.write(image.screenshot_as_png)
-    '''
+        # image = chrome.find_element_by_xpath('//*[@id="outputEl"]/div/div/img')
+        # file.write(image.screenshot_as_png)
+        file.write(imgdata)
 
     client = ImgurClient(client_id, client_secret, access_token, refresh_token)
     config = {
